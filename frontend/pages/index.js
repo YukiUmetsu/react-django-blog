9864f6215db4ff97aa0cfa837f29272d9ada27f0
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-03-18 00:21:07 -0500

    initial commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+export default Index;

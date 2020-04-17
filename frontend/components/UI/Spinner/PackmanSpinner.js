976e0f8869866cd759da0ddd4572d1a2aad90c03
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-04-17 09:18:24 -0500

    added packman spinner

---
+++ frontend/components/UI/Spinner/PackmanSpinner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './PackmanSpinner.module.css';
+
+const PackmanSpinner = (props) => {
+    return (
+        <div className={styles['spinner-bean-eater']}>
+            <div className={styles['bean']}>
+                <div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                </div>
+                <div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PackmanSpinner

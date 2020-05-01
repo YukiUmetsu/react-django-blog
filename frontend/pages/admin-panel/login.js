ada402e019d7fc1184c2822c7dff382379408477
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-05-01 14:39:37 -0500

    fixed admin login ui issue

--- frontend/pages/admin-panel/login.js
+++ frontend/pages/admin-panel/login.js
@@ -68,7 +68,7 @@ const AdminLogin = (props) => {
 
     return (
         <div className="h-screen overflow-hidden flex items-center justify-center" style={{background: '#edf2f7'}}>
-            <div className="bg-grey-lighter min-h-screen flex flex-col min-w-full sm:w-full md:min-w-1/2 lg:min-w-1/3 xl:min-w-1/3">
+            <div className="bg-grey-lighter min-h-screen flex flex-col min-w-full sm:w-/13 md:min-w-1/2 lg:min-w-1/3 xl:min-w-1/3">
                 { renderAlert() }
                 <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                     <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
